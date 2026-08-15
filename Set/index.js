a6ba2295ab62d3f00224160320ff525c90923c03
Anon873-cyber <async2304@gmail.com>
2026-08-16 00:08:36 +0530

    refactor constructor to improve clarity and ensure unique elements are added to the Set

--- Set/index.js
+++ Set/index.js
@@ -1,7 +1,23 @@
 class Set {
 
     // getting objects or
-    constructor(obj) {
-        
+    constructor(object) {
+        // pointing the object ref to empty array
+        this.object = [];
+        // inserting the unique object to the empty array
+        for (let index = 0; index < object.length; index++) {
+            const element = object[index];
+            // inserting the element only if it not already presnt in the existing array
+            if (!this.object.includes(element)) {
+                //pushing the element at the end of the object
+                this.object.push(element)
+            }
+        }
+        //returning the object
+          return this.object
+
     }
 }
+
+let x = new Set([1, 3, 3, 4, 5])
+console.log(x)
