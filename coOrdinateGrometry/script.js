c3428b74de49f633e9001eaca41adf81f2e58cd2
Anon873-cyber <async2304@gmail.com>
2026-08-20 20:44:32 +0530

    add initial HTML and JavaScript files for Coordinate Geometry visualization

---
+++ coOrdinateGrometry/script.js
@@ -0,0 +1,15 @@
+// creating a quard 
+var trace2 = {
+  x: [0,10,20,20,],
+  y: [40,40,40,0],
+  type: 'scatter'
+};
+// findting the area of it 
+// area of quard is side * side so 
+//  20 * 40 = 800 unit sq
+
+
+
+var data = [trace2];
+
+Plotly.newPlot('graph', data);
